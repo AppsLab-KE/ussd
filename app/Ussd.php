dd5be27d3e25cec821633f1ae070b64b3f5434f7
kodaTheCoder <marvincollins14@gmail.com>
2020-03-11 02:03:39 +0300

    starting a basic ussd project

---
+++ app/Ussd.php
@@ -0,0 +1,10 @@
+<?php
+namespace AppsLab\Ussd;
+
+class Ussd
+{
+    public function index()
+    {
+        echo "hello world";
+    }
+}
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+use AppsLab\Ussd\Ussd;
+
+require_once('vendor/autoload.php');
+
+$ussd = new Ussd();
+
+var_dump($ussd);
+
+if($_GET)
